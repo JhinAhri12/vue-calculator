cfd4e1988f5f5b65c839e667f4aab251c7588e68
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-08-03 13:30:10 +0200

    bouton fonctionnel

--- src/App.vue
+++ src/App.vue
@@ -14,77 +14,72 @@
 
       <div class="d-flex ">
         <div class="col-1 offset-4 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">C</button>
+          <button @click.prevent="reset" class="btn btn-secondary btn-lg m-1">C</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">+/-</button>
+          <button @click.prevent="number" class="btn btn-secondary btn-lg m-1">+/-</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">%</button>
+          <button @click.prevent="modulo" class="btn btn-secondary btn-lg m-1">%</button>
         </div><br>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-success btn-lg m-1">+</button>
+          <button @click.prevent="divide" class="btn btn-success btn-lg m-1">/</button>
         </div>
       </div>
       <div class="d-flex ">
         <div class="col-1 offset-4 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">7</button>
+          <button @click.prevent="seven" class="btn btn-secondary btn-lg m-1">7</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">8</button>
+          <button @click.prevent="eigth" class="btn btn-secondary btn-lg m-1">8</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">9</button>
+          <button @click.prevent="nine" class="btn btn-secondary btn-lg m-1">9</button>
         </div><br>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-success btn-lg m-1">*</button>
+          <button @click.prevent="multiply" class="btn btn-success btn-lg m-1">*</button>
         </div>
       </div>
       <div class="d-flex ">
         <div class="col-1 offset-4 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">4</button>
+          <button @click.prevent="four" class="btn btn-secondary btn-lg m-1">4</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">5</button>
+          <button @click.prevent="five" class="btn btn-secondary btn-lg m-1">5</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">6</button>
+          <button @click.prevent="six" class="btn btn-secondary btn-lg m-1">6</button>
         </div><br>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-success btn-lg m-1">-</button>
+          <button @click.prevent="minus" class="btn btn-success btn-lg m-1">-</button>
         </div>
       </div>
       <div class="d-flex ">
         <div class="col-1 offset-4 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">1</button>
+          <button @click.prevent="one" class="btn btn-secondary btn-lg m-1">1</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">2</button>
+          <button @click.prevent="two" class="btn btn-secondary btn-lg m-1">2</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">3</button>
+          <button @click.prevent="three" class="btn btn-secondary btn-lg m-1">3</button>
         </div><br>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-success btn-lg m-1">+</button>
+          <button @click.prevent="add" class="btn btn-success btn-lg m-1">+</button>
         </div>
       </div>
       <div class="d-flex ">
         <div class="col-2 offset-4 d-grid gap-2">
-          <button class="btn btn-secondary btn-lg m-1">0</button>
+          <button @click.prevent="zero" class="btn btn-secondary btn-lg m-1">0</button>
         </div>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-secondary btn-lg m-1">.</button>
+          <button @click.prevent="dot" class="btn btn-secondary btn-lg m-1">.</button>
         </div><br>
         <div class="col-1 d-grid gap-1">
-          <button class="btn btn-success btn-lg m-1">=</button>
+          <input type="submit" class="btn btn-success btn-lg m-1" value="=">
         </div>
       </div>
-      <br>
-      
-      <input type="text" v-model="query">
-      <input type="submit" value="=">
-      
- 
+      <br> 
     </form>
   </div>
 </div>
@@ -97,7 +92,6 @@
 </template>
 
 <style scoped>
-
 </style>
 
 <script>
@@ -111,6 +105,60 @@ export default {
     }
   },
   methods : {
+    add(){
+      this.query += "+"
+    },
+    zero(){
+      this.query += "0"
+    },
+    one(){
+      this.query += "1"
+    },
+    two(){
+      this.query += "2"
+    },
+    three(){
+      this.query += "3"
+    },
+    four(){
+      this.query += "4"
+    },
+    five(){
+      this.query += "5"
+    },
+    six()
+    {
+      this.query += "6"
+    },
+    seven(){ 
+      this.query += "7"
+    },
+    eight(){
+      this.query += "8"
+    },
+    nine(){
+      this.query += "9"
+    },
+    reset() {
+      this.query = '',
+      this.calculations = [],
+      this.result = ''
+    },
+    divide() {
+      this.query += '/'
+    },
+    multiply(){
+      this.query += '*'
+    },
+    modulo(){
+      this.query += '%'
+    },
+    dot(){
+      this.query += '.'
+    },
+    number(){
+      this.query += '-'
+    },
     myResult() {
       this.result = eval(this.query)
       this.calculations.push({
